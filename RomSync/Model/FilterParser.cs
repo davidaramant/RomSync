a6f8cc2039aa4e010ef80f781d9e4efc7a357757
David Aramant <davidaramant@gmail.com>
2016-05-23 22:10:46 -0400

    A simple filter parser

---
+++ RomSync/Model/FilterParser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomSync.Model
+{
+    public static class FilterParser
+    {
+        public static IEnumerable<string> Parse(string input)
+        {
+            return input.ToLowerInvariant().Split(new [] {' '},StringSplitOptions.RemoveEmptyEntries);
+        }
+    }
+}
---
+++ Tests/FilterParserTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using RomSync.Model;
+
+namespace Tests
+{
+    [TestFixture]
+    public sealed class FilterParserTests
+    {
+        [Test]
+        public void ShouldLowerCaseInput()
+        {
+            var output = FilterParser.Parse("SomeTHING");
+
+            Assert.That(output.ToArray(), Is.EqualTo(new[] { "something"}));
+        }
+
+        [Test]
+        public void ShouldSplitOnWhitespace()
+        {
+            var output = FilterParser.Parse("  Here Is Some Stuff");
+
+            Assert.That(output.ToArray(), Is.EqualTo(new[] {"here","is","some","stuff"}));
+        }
+    }
+}
